d233a87154eaf60f3e2f5a06378e9e5feab0cc87
Jake Martin <55101169+jpmart12@users.noreply.github.com>
2020-12-28 18:58:11 -0500

    Update to include app_home_opened event

--- bot2.py
+++ bot2.py
@@ -16,10 +16,56 @@ def say_hello(message, say):
 def ask_who(message, say):
     say("_Who's there?_")
 
-def main():
-    app.start(port=int(os.environ.get("PORT", 3000)))
-    return
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
 
 # Start your app
 if __name__ == "__main__":
-    main()
+    app.start(port=int(os.environ.get("PORT", 3000)))
